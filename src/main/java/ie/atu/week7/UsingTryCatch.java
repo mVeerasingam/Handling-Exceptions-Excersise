0c94199495f6fcd050339bc617f604887cf85384
G00403931 <g00403931@atu.ie>
2022-11-08 14:21:32 +0000

    fin code

--- src/main/java/ie/atu/week7/UsingTryCatch.java
+++ src/main/java/ie/atu/week7/UsingTryCatch.java
@@ -5,17 +5,22 @@ import java.util.Scanner;
 
 public class UsingTryCatch {
     public static void main(String[] args) {
-        System.out.println("Please enter a number between 0-9 : ");
+
         Scanner myscan = new Scanner(System.in);
-        try {
-            int myNum = myscan.nextInt();
-            if (myNum <= 9 && myNum > 0) {
-                System.out.println("You Entered " + myNum);
-            } else {
-                System.out.println("Not a valid number");
+        while(true) {
+            System.out.println("Please enter a number between 0-9 : ");
+            try {
+                int myNum = myscan.nextInt();
+                if (myNum <= 9 && myNum > 0) {
+                    System.out.println("You Entered " + myNum);
+                    break;
+                } else {
+                    System.out.println("Not a valid number");
+                }
+            } catch (InputMismatchException ime) {
+                System.out.println("error please try again");
+                myscan.next();
             }
-        }catch (InputMismatchException ime){
-            System.out.println("error");
         }
     }
 }
